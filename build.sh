df2b5b8109e64486a002f67b0dcc0a32d0eb3502
Thomas N <atom@fortytwo.fr>
2025-02-09 14:27:43 +0100

    feat: pack firmware with configuration and build info

--- build.sh
+++ build.sh
@@ -20,11 +20,11 @@ Arguments :
 -p|--platform : Platform to be used by platformio to perform the build (e.g. 'STM32F103RC_creality')
 
 [Optional]
--s|--src-ref : Marlin firmware source revision, can be a commit, a tag, a branch (defaults to 'bugfix-2.1.x')
+-s|--src-ref : Marlin firmware source revision, can be a commit, a tag, a branch (defaults to '2.1.2.5')
 
 -C|--cfg-ref : Marlin firmware configuration revision, can be a commit, a tag, a branch (defaults to Marlin's source revision)
 
--d|--docker-builder-ref : Marlin firmware docker builder revision, can be a commit, a tag, a branch, but you probably want to use a tag (defaults to '2.1.2')
+-d|--docker-builder-ref : Marlin firmware docker builder revision, can be a commit, a tag, a branch, but you probably want to use a tag (defaults to '2.1.2.5')
 
 -o|--output : Output filename (defaults to "Marlin-<marlin-source-ref>-<platform>_<date>.bin")
 EOM
@@ -80,16 +80,16 @@ done
 ## Variables, tweak these as you like or provide values at runtime ##
 
 # Default Marlin source code ref to fetch
-[[ -z ${MARLIN_SRC_REF} ]] && MARLIN_SRC_REF="bugfix-2.1.x"
+[[ -z ${MARLIN_SRC_REF} ]] && MARLIN_SRC_REF="2.1.2.5"
 
 # Default Marlin configuration ref to fetch
 [[ -z ${MARLIN_CFG_REF} ]] && MARLIN_CFG_REF=${MARLIN_SRC_REF}
 
-# Default Marlin docker-compose.yml file ref to fetch
-[[ -z ${MARLIN_DOCKER_BUILDER_REF} ]] && MARLIN_DOCKER_BUILDER_REF="2.1.2"
+# Default Marlin Dockerfile ref to fetch
+[[ -z ${MARLIN_DOCKER_BUILDER_REF} ]] && MARLIN_DOCKER_BUILDER_REF="2.1.2.5"
 
-# Default Artefact name, this is the file you will flash on your printer
-[[ -z ${FIRMWARE_NAME} ]] && FIRMWARE_NAME="Marlin-${MARLIN_SRC_REF}-${PLATFORM}_$(date +'%Y%m%d%H%M%S').bin"
+# Default Artifact name, this will be a tar.xz archive containing the .bin firmware to flash as well as the configuration used and build info.
+[[ -z ${FIRMWARE_NAME} ]] && FIRMWARE_NAME="Marlin-${MARLIN_SRC_REF}-${PLATFORM}_$(date +'%Y%m%d%H%M%S')"
 
 # Marlin source repo, you probably don't need to change this
 MARLIN_SRC_REPO=https://github.com/MarlinFirmware/Marlin.git
@@ -105,14 +105,13 @@ MARLIN_CFG_REPO=https://github.com/MarlinFirmware/Configurations.git
 
 command -v git >/dev/null 2>&1 || { printf "\n\033[0;31mYou need git\033[0m\n"; exit 1; }
 command -v docker >/dev/null 2>&1 || { printf "\n\033[0;31mYou need docker\033[0m\n"; exit 1; }
-docker compose version >/dev/null 2>&1 || { printf "\n\033[0;31mYou need docker compose plugin\033[0m\n"; exit 1; }
 [[ -z ${MARLIN_SRC_REPO} ]] && { printf "\n\033[0;31mYou need to set MARLIN_SRC_REPO variable\033[0m\n"; exit 1; }
 [[ -z ${MARLIN_CFG_REPO} ]] && { printf "\n\033[0;31mYou need to set MARLIN_CFG_REPO variable\033[0m\n"; exit 1; }
 [[ -z ${MARLIN_SRC_REF} ]] && { printf "\n\033[0;31mYou need to set MARLIN_SRC_REF variable\033[0m\n"; exit 1; }
 [[ -z ${MARLIN_CFG_REF} ]] && MARLIN_CFG_REF=${MARLIN_SRC_REF}
 [[ -z ${MARLIN_DOCKER_BUILDER_REF} ]] && MARLIN_DOCKER_BUILDER_REF=${MARLIN_SRC_REF}
-[[ -z ${MARLIN_CFG_SUBFOLDER} ]] && { printf "\n\033[0;31mYou need to set MARLIN_CFG_SUBFOLDER variable\033[0m\n"; exit 1; }
-[[ -z ${PLATFORM} ]] && { printf "\n\033[0;31mYou need to set PLATFORM variable\033[0m\n"; exit 1; }
+[[ -z ${MARLIN_CFG_SUBFOLDER} ]] && { usage; exit 1; }
+[[ -z ${PLATFORM} ]] && { usage; exit 1; }
 [[ -z ${FIRMWARE_NAME} ]] && { printf "\n\033[0;31mYou need to set FIRMWARE_NAME variable\033[0m\n"; exit 1; }
 
 WORKING_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
@@ -157,40 +156,53 @@ if [[ ! $(git -C ${TMP_DCK} remote -v | grep origin | grep fetch | awk '{print $
   echo "Cloning Marlin from GitHub to ${TMP_DCK}"
   git clone --depth=1 --single-branch --branch "${MARLIN_DOCKER_BUILDER_REF}" ${MARLIN_SRC_REPO} "${TMP_DCK}" || { printf "\n\033[0;31mFailed to clone Marlin Docker builder\033[0m\n"; exit 1; }
 else
-  echo "Using cached docker-compose.yml"
+  echo "Using cached Dockerfile"
   git -C ${TMP_DCK} reset --hard HEAD
 fi
 
-# Configure build #
-
+# Configure #
 printf "\n\033[0;32mInjecting Configuration\033[0m\n"
 
 cp --remove-destination --verbose config/*.h ${TMP_SRC}/Marlin/
 
 printf "\n\033[0;32mSetting up Docker\033[0m\n"
 
-cp --remove-destination --verbose ${TMP_DCK}/docker-compose.yml ${TMP_SRC}/
 cp --remove-destination --verbose -r ${TMP_DCK}/docker ${TMP_SRC}/
 
 cd "${TMP_SRC}"
+pwd
+docker build . -t marlin
+docker run --rm -v .:/code marlin /code/buildroot/bin/format_code || true
 
-docker compose build
-docker compose run --rm marlin /code/buildroot/bin/format_code || true
-
+# Build #
 printf "\n\033[0;32mCompiling Marlin for %s\033[0m\n" "${PLATFORM}"
 
-docker compose run --rm marlin platformio run --target clean -e "${PLATFORM}"
-docker compose run --rm marlin platformio run -e "${PLATFORM}" --silent
+docker run --rm -v .:/code marlin pio run --target clean -e "${PLATFORM}"
+docker run --rm -v .:/code marlin pio run -e "${PLATFORM}" --silent
 
+# Pack #
 printf "\n\033[0;32mCopying compiled firmware\033[0m\n"
 
-find "${TMP_SRC}/.pio/build/${PLATFORM}" -name "firmware-*.bin" -exec cp --remove-destination --verbose '{}' "${WORKING_DIR}/${FIRMWARE_NAME}" \;
-if [[ ! -e "${WORKING_DIR}/${FIRMWARE_NAME}" ]]; then
+find "${TMP_SRC}/.pio/build/${PLATFORM}" -name "firmware-*.bin" -exec cp --remove-destination --verbose '{}' "${WORKING_DIR}/${FIRMWARE_NAME}.bin" \;
+if [[ ! -e "${WORKING_DIR}/${FIRMWARE_NAME}.bin" ]]; then
   printf "\n\033[0;31m => Firmware compilation failed, check logs <=\033[0m\n\n"
   exit 1
 fi
 
+echo "MARLIN_SRC_REPO=${MARLIN_SRC_REPO}" > "${WORKING_DIR}/build-info.txt"
+echo "MARLIN_SRC_REF=${MARLIN_SRC_REF}" >> "${WORKING_DIR}build-info.txt"
+echo "MARLIN_CFG_REPO=${MARLIN_CFG_REPO}" >> "${WORKING_DIR}/build-info.txt"
+echo "MARLIN_CFG_REF=${MARLIN_CFG_REF}" >> "${WORKING_DIR}/build-info.txt"
+echo "MARLIN_DOCKER_BUILDER_REF=${MARLIN_DOCKER_BUILDER_REF}" >> "${WORKING_DIR}/build-info.txt"
+echo "MARLIN_CFG_SUBFOLDER=${MARLIN_CFG_SUBFOLDER}" >> "${WORKING_DIR}/build-info.txt"
+
+cd "${WORKING_DIR}"
+tar cvJf "${FIRMWARE_NAME}.tar.xz" build-info.txt "${FIRMWARE_NAME}.bin" config/*.h
+
 printf "\n\033[0;32mFirmware successfully compiled\033[0m\n"
 
-printf "\n\033[0;32m => ${FIRMWARE_NAME} <=\033[0m\n\n"
+# Cleanup #
+rm -f "${WORKING_DIR}/${FIRMWARE_NAME}.bin" "${WORKING_DIR}/build-info.txt"
+
+printf "\n\033[0;32m => ${FIRMWARE_NAME}.tar.xz <=\033[0m\n\n"
 
